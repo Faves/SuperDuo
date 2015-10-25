cfc3762f9d8aff3434e1f6f2f7970523f31537ec
Faves <Fabien.vorillion@gmauil.com>
2015-10-25 12:00:24 +0100

    Alexandria - fix   -> DownloadImage : Checks if the image url is not empty

--- alexandria/app/src/main/java/it/jaschke/alexandria/services/DownloadImage.java
+++ alexandria/app/src/main/java/it/jaschke/alexandria/services/DownloadImage.java
@@ -3,6 +3,7 @@ package it.jaschke.alexandria.services;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
+import android.text.TextUtils;
 import android.util.Log;
 import android.widget.ImageView;
 
@@ -19,14 +20,19 @@ public class DownloadImage extends AsyncTask<String, Void, Bitmap> {
     }
 
     protected Bitmap doInBackground(String... urls) {
-        String urlDisplay = urls[0];
+        String urlDisplay = (urls.length > 0 ? urls[0] : null);
         Bitmap bookCover = null;
-        try {
-            InputStream in = new java.net.URL(urlDisplay).openStream();
-            bookCover = BitmapFactory.decodeStream(in);
-        } catch (Exception e) {
-            Log.e("Error", e.getMessage());
-            e.printStackTrace();
+
+        //Checks if the image url is not empty
+        //FIX : java.net.MalformedURLException: Protocol not found
+        if (!TextUtils.isEmpty(urlDisplay)) {
+            try {
+                InputStream in = new java.net.URL(urlDisplay).openStream();
+                bookCover = BitmapFactory.decodeStream(in);
+            } catch (Exception e) {
+                Log.e("Error", e.getMessage());
+                e.printStackTrace();
+            }
         }
         return bookCover;
     }
