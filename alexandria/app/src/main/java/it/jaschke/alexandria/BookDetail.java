5d18a4fedb4591841a47694ebc8994985fc012ac
Faves <Fabien.vorillion@gmail.com>
2015-10-27 18:52:48 +0100

    Alexandria - fix the case when a book doesn't have any authors else this line will crash with a null pointer exception. Try ISBN 9781412714990.

--- alexandria/app/src/main/java/it/jaschke/alexandria/BookDetail.java
+++ alexandria/app/src/main/java/it/jaschke/alexandria/BookDetail.java
@@ -160,9 +160,9 @@ public class BookDetail extends Fragment implements LoaderManager.LoaderCallback
         ((TextView) rootView.findViewById(R.id.fullBookDesc)).setText(desc);
 
         String authors = data.getString(data.getColumnIndex(AlexandriaContract.AuthorEntry.AUTHOR));
-        String[] authorsArr = authors.split(",");
-        ((TextView) rootView.findViewById(R.id.authors)).setLines(authorsArr.length);
-        ((TextView) rootView.findViewById(R.id.authors)).setText(authors.replace(",","\n"));
+        String[] authorsArr = (!TextUtils.isEmpty(authors) ? authors.split(",") : null);
+        ((TextView) rootView.findViewById(R.id.authors)).setLines(authorsArr != null ? authorsArr.length : 0);
+        ((TextView) rootView.findViewById(R.id.authors)).setText(authors != null ? authors.replace(",","\n") : "");
         String imgUrl = data.getString(data.getColumnIndex(AlexandriaContract.BookEntry.IMAGE_URL));
         if(Patterns.WEB_URL.matcher(imgUrl).matches()){
             mPicasso.load(imgUrl)
